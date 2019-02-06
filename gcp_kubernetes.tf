7ba227ab4db3ad2a255c56e1d5e0412f38d48df0
Allan Eayrs <ae.eayrs@gmail.com>
2019-02-06 11:46:58 -0800

    fixed oauth_scopes name

---
+++ gcp_kubernetes.tf
@@ -0,0 +1,27 @@
+resource "google_container_cluster" "kubey" {
+  name = "kubeycluster"
+  zone = "us-west-1a" 
+  initial_node_count = "1"
+  additional_zones = [
+    "us-west-1b"
+  ]
+  master_auth = {
+    password = "password"
+    username = "username"
+  }
+
+  node_config {
+    oauth_scopes = [
+     "https://www.googleapis.com/auth/compute",
+     "https://www.googleapis.com/auth/devstorage.read_only",
+     "https://www.googleapis.com/auth/logging.write",
+     "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels {
+      this-is-for = "dev-cluster"
+    }
+
+    tags = ["dev", "work"]
+  }
+}
