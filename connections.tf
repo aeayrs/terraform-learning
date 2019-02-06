27c3717478c309c15765135aecaf8af6ae1580fe
Allan Eayrs <ae.eayrs@gmail.com>
2019-02-06 10:07:25 -0800

    added aws_security_group

--- connections.tf
+++ connections.tf
@@ -7,7 +7,7 @@ provider "aws" {
     region = "us-west-2" 
 }
 provider "azurerm" {
-    subsceiption_id = "0"
+    subscription_id = "0"
     client_id = "1"
     client_secret = "2"
     tenant_id = "3"
